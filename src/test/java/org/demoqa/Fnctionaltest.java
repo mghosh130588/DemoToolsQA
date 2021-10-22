781036bfddda1b40562d34a78efaef309cca8c4a
Mousumi Ghosh <mousumi.nit@gmail.com>
2021-10-22 22:50:00 +0200

    second commit

--- src/test/java/org/demoqa/Fnctionaltest.java
+++ src/test/java/org/demoqa/Fnctionaltest.java
@@ -1,4 +1,23 @@
 package org.demoqa;
 
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 public class Fnctionaltest {
+
+    @BeforeTest
+    public void beforTest(){
+        System.out.println("This is before test");
+    }
+    @Test
+    public void testqa(){
+        System.out.println("this is test");
+    }
+
+    @AfterTest
+    public void aftertest(){
+        System.out.println("This is after test");
+    }
 }
