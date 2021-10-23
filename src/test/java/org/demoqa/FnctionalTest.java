c8175a733bf2bb54adc2fb8b693e15b8e6b4c976
Mousumi Ghosh <mousumi.nit@gmail.com>
2021-10-23 10:29:58 +0200

    pom chnaged added

--- src/test/java/org/demoqa/FnctionalTest.java
+++ src/test/java/org/demoqa/FnctionalTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class Fnctionaltest {
+public class FnctionalTest {
 
     @BeforeTest
     public void beforTest(){
